0bfc6a8c865f157ebc78942d5d3357825e48ebd8
acanois <david.richter42@gmail.com>
2024-01-14 14:33:21 -0600

    three of them

--- src/app/components/BabylonScene.tsx
+++ src/app/components/BabylonScene.tsx
@@ -12,11 +12,11 @@ import SceneComponent from 'babylonjs-hook'
 import { Attractor } from '../scripts/attractor'
 import { MovingBody } from '../scripts/movingBody'
 
-let attractor: Attractor
+let attractors: Attractor[] = []
 let body: MovingBody
 
 const onSceneReady = (scene: Scene) => {
-  scene.clearColor = new Color4(0, 0, 0, 1.0)
+  scene.clearColor = new Color4(0.15, 0.15, 0.3, 1.0)
 
   const canvas = scene.getEngine().getRenderingCanvas()
   canvas!.width = window.innerWidth
@@ -24,7 +24,7 @@ const onSceneReady = (scene: Scene) => {
 
   const camera: ArcRotateCamera = new ArcRotateCamera(
     'Camera',
-    Math.PI / 2,
+    -Math.PI / 2,
     Math.PI / 2,
     30, // Camera distance
     Vector3.Zero(),
@@ -37,22 +37,54 @@ const onSceneReady = (scene: Scene) => {
   const light2 = new HemisphericLight('light2', new Vector3(1, -1, -1), scene)
   light2.intensity = 0.7
 
-  attractor = new Attractor(new Vector3(0, 0, 0), 1.0, scene)
-
-  body = new MovingBody(
-    new Vector3(3.0, 0.0, 3.0), // position
-    new Vector3(-0.01, 0.05, 0.0), // velocity
-    new Vector3(0.0, 0.0, 0.0), // acceleration
-    0.5, // mass
-    0.5, // radius
-    scene
+  attractors.push(
+    // Top
+    new Attractor(
+      new Vector3(-3, 3, 0), // position
+      new Vector3(0.01, 0.01, 0.0), // velocity
+      new Vector3(0.0, 0.0, 0.0), // acceleration
+      1.0,
+      1.0,
+      scene
+    )
   )
+  
+  // Bottom Left
+  attractors.push(
+    new Attractor(
+      new Vector3(-3, -3, 0), // position
+      new Vector3(-0.01, 0.01, 0.0), // velocity
+      new Vector3(0.0, 0.0, 0.0), // acceleration
+      1.0,
+      1.0,
+      scene
+    )
+  )
+  
+  // Bottom Right
+  attractors.push(
+    new Attractor(
+      new Vector3(3, -3, 0), // position
+      new Vector3(-0.01, -0.01, 0.0), // velocity
+      new Vector3(0.0, 0.0, 0.0), // acceleration
+      1.0,
+      1.0,
+      scene
+    )
+  )
+      
 }
 
 const onRender = (scene: Scene) => {
-  const force = attractor.attract(body)
-  body.update()
-  body.applyForce(force)
+  attractors.forEach((attractor, i) => {
+    attractor.update()
+    const previous = i - 1 < 0 ? attractors.length - 1 : i - 1
+    const next = i + 1 > (attractors.length - 1) ? 0 : i + 1
+    const force = attractor.attract(attractors[next])
+    const force2 = attractor.attract(attractors[previous])
+    attractors[next].applyForce(force)
+    attractors[previous].applyForce(force2)
+  })
 }
 
 function App() {
--- src/app/scripts/attractor.ts
+++ src/app/scripts/attractor.ts
@@ -1,61 +1,84 @@
 import {
-    Scene,
-    Vector3,
-    MeshBuilder,
-    StandardMaterial,
-    Color3,
-    Mesh,
-  } from '@babylonjs/core'
-  
-  import { MovingBody } from './movingBody'
-  
-  export class Attractor {
-    position: Vector3
-    mass: number
-    mesh: Mesh
+  Scene,
+  Vector3,
+  MeshBuilder,
+  StandardMaterial,
+  Color3,
+  Mesh,
+} from '@babylonjs/core'
+
+import { MovingBody } from './movingBody'
+
+export class Attractor {
+  position: Vector3
+  velocity: Vector3
+  acceleration: Vector3
+  mass: number
+  radius: number
+  mesh: Mesh
+  scene: Scene
+
+  constructor(
+    position: Vector3,
+    velocity: Vector3,
+    acceleration: Vector3,
+    mass: number,
+    radius: number,
     scene: Scene
-  
-    constructor(position: Vector3, mass: number, scene: Scene) {
-      this.position = position
-      this.mass = mass
-      this.scene = scene
-      this.mesh = this.createMesh()
-    }
-  
-    setMag(inputVector: Vector3, targetMag: number) {
-      const currentMag = inputVector.length()
-      const x = inputVector.x * (targetMag / currentMag)
-      const y = inputVector.y * (targetMag / currentMag)
-      const z = inputVector.z * (targetMag / currentMag)
-  
-      return new Vector3(x, y, z)
-    }
-  
-    attract(body: MovingBody): Vector3 {
-      const G = 0.01
-      const force = this.position.subtract(body.position)
-      const gravity = (G * this.mass * body.mass) / (force.lengthSquared())
-      
-      return this.setMag(force, gravity)
-    }
-   
-    createMesh(): Mesh {
-      const sphere = MeshBuilder.CreateSphere(
-        'mover',
-        {
-          diameter: this.mass,
-        },
-        this.scene
-      )
-      sphere.position = this.position
-      const sphereMaterial: StandardMaterial = new StandardMaterial(
-        'moverMaterial',
-        this.scene
-      )
-      sphereMaterial.diffuseColor = new Color3(1, 0.3, 0.3)
-      sphere.material = sphereMaterial
-  
-      return sphere
-    }
+  ) {
+    this.position = position
+    this.velocity = velocity
+    this.acceleration = acceleration
+    this.mass = mass
+    this.radius = radius
+    this.scene = scene
+    this.mesh = this.createMesh()
   }
-  
+
+  setMag(inputVector: Vector3, targetMag: number) {
+    const currentMag = inputVector.length()
+    const x = inputVector.x * (targetMag / currentMag)
+    const y = inputVector.y * (targetMag / currentMag)
+    const z = inputVector.z * (targetMag / currentMag)
+
+    return new Vector3(x, y, z)
+  }
+
+  attract(body: Attractor): Vector3 {
+    const G = 0.001
+    const force = this.position.subtract(body.position)
+    const gravity = (G * this.mass * body.mass) / force.lengthSquared()
+
+    return this.setMag(force, gravity)
+  }
+
+  update() {
+    this.velocity.addInPlace(this.acceleration)
+    this.position.addInPlace(this.velocity)
+    this.acceleration.multiplyInPlace(Vector3.Zero())
+  }
+
+  applyForce(force: Vector3) {
+    const f = force.scale(1 / this.mass)
+    this.acceleration = this.acceleration.add(f)
+  }
+
+  createMesh(): Mesh {
+    const sphere = MeshBuilder.CreateSphere(
+      'mover',
+      {
+        diameter: this.mass,
+      },
+      this.scene
+    )
+    sphere.position = this.position
+    const sphereMaterial: StandardMaterial = new StandardMaterial(
+      'moverMaterial',
+      this.scene
+    )
+    sphereMaterial.diffuseColor = new Color3(0.3, 0.8, 0.3)
+    sphere.material = sphereMaterial
+
+    return sphere
+  }
+}
